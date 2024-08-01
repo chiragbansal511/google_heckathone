eeb5e21f40125e271955e87c088f0b89126dac88
chirag bansal <chirageng.stu22@gmail.com>
2024-08-01 22:55:10 +0530

    yeild model connected

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask,request,render_template
 import numpy as np
+import pandas
+import sklearn
 import pickle
 
 # importing model
-model = pickle.load(open('model.pkl','rb'))
-sc = pickle.load(open('standscaler.pkl','rb'))
-ms = pickle.load(open('minmaxscaler.pkl','rb'))
+model = pickle.load(open('crop_predction_model.pkl','rb'))
+sc = pickle.load(open('crop_predction_standscaler.pkl','rb'))
+ms = pickle.load(open('crop_predction_minmaxscaler.pkl','rb'))
 
 # creating flask app
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to the Crop Prediction API"
+    return render_template("index.html")
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict",methods=['POST'])
 def predict():
-    # Getting form data
     N = request.form['Nitrogen']
     P = request.form['Phosporus']
     K = request.form['Potassium']
@@ -25,29 +26,27 @@ def predict():
     ph = request.form['Ph']
     rainfall = request.form['Rainfall']
 
-    # Preparing the feature array
     feature_list = [N, P, K, temp, humidity, ph, rainfall]
     single_pred = np.array(feature_list).reshape(1, -1)
 
-    # Applying scalers
     scaled_features = ms.transform(single_pred)
     final_features = sc.transform(scaled_features)
     prediction = model.predict(final_features)
 
-    # Crop dictionary
-    crop_dict = {1: "Rice", 2: "Maize", 3: "Chickpea", 4: "Kidneybeans", 5: "Pigeonpeas", 6: "Mothbeans", 7: "Mungbean",
-                 8: "Blackgram", 9: "Lentil", 10: "Pomegranate", 11: "Banana", 12: "Mango", 13: "Grapes",
-                 14: "Watermelon", 15: "Muskmelon", 16: "Apple", 17: "Orange", 18: "Papaya",
-                 19: "Coconut", 20: "Cotton", 21: "Jute", 22: "Coffee"}
+    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
 
-    # Returning JSON response
     if prediction[0] in crop_dict:
         crop = crop_dict[prediction[0]]
         result = "{} is the best crop to be cultivated right there".format(crop)
     else:
         result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
+    return render_template('index.html',result = result)
 
-    return jsonify({"prediction": result})
+
+
 
 # python main
 if __name__ == "__main__":
